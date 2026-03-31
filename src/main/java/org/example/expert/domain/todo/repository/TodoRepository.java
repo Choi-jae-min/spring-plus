648dbe488ddd8a05d00ed53cb61ded639e9f7429
jasmin <cjm98@smartel.co.kr>
2026-03-31 14:32:02 +0900

    feat : JPQL 활용 Todo에 날짜,날씨 검색기능 추가[Lv.1-3]

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("""
+              SELECT t FROM Todo t LEFT JOIN FETCH t.user u
+              WHERE (:weather IS NULL OR t.weather = :weather)
+              AND (:updateStartAt IS NULL OR t.modifiedAt >= :updateStartAt)
+              AND (:updateEndAt IS NULL OR t.modifiedAt <= :updateEndAt)
+              ORDER BY t.modifiedAt DESC
+            """)
+    Page<Todo> findAllBySearchByOrderByModifiedAtDesc(Pageable pageable,
+                                                      @Param("weather") String weather,
+                                                      @Param("updateStartAt") LocalDateTime updateStartAt,
+                                                      @Param("updateEndAt") LocalDateTime updateEndAt);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
